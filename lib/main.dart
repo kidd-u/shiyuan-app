df706582702a95bb412b6a5b175205f255370ae5
gengyuanfeng <970165187@qq.com>
2020-05-16 11:43:07 +0800

    new

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'Home.dart';
+import 'Library.dart';
+import 'Mine.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          brightness: Brightness.light, backgroundColor: Colors.white),
+      home: ScaffoldRoute(),
+    );
+  }
+}
+
+class ScaffoldRoute extends StatefulWidget {
+  @override
+  _ScaffoldRouteState createState() => _ScaffoldRouteState();
+}
+
+class _ScaffoldRouteState extends State<ScaffoldRoute> {
+  int _selectedIndex = 0;
+  var pageList = [new HomePage(), new LibraryPage(), new MinePage()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pageList[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        // 底部导航
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.business), title: Text('Business')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.people), title: Text('School')),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
